ec1d4f47ebf5af0da393e2afd61c295c355d0b68
Adilet <amirden2003@gmail.com>
2021-06-03 21:55:26 +0600

    added docs

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
   },
   output: {
     filename: "[name].[contenthash].js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "docs", "dist"),
   },
   resolve: {
     extensions: [".js", ".json", ".png", ".jpg"],
